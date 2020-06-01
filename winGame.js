f58e6c9cc26dd24d62ccafcb5dcb798e49d10897
Rama Sabajo <rsabajo@gmail.com>
2020-06-01 14:21:35 +0200

    winGame function en test SUCCES!

--- winGame.js
+++ winGame.js
@@ -1,6 +1,15 @@
 const winGame = () => {
-  const Letterlist = ['h', 'o', 'n', 'd'];
+  const correctLetters = ['h', 'o', 'n', 'd'];
   const word = 'hond';
+
+  if (
+    correctLetters.every(letter => word.includes(letter)) &&
+    word.length == correctLetters.length
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = winGame;
